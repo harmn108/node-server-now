ced20175e12bb8840f1a8f170e0e4872b07e42f6
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-03-04 18:34:52 +0400

    add user id route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
   res.end('hello world');
 });
 
+app.get('/user/:id', function(req, res) {
+  res.end('hello user with id: ' + req.params.id);
+});
+
 app.get('/:id', function(req, res) {
     res.end('hello hutul with id: ' + req.params.id);
 });
