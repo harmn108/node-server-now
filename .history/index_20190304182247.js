04ce56d0eb5310f8a32aa82b34529f3beede6d29
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-03-04 18:28:08 +0400

    changes

--- .history/index_20190304182247.js
+++ .history/index_20190304182247.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.end('hello world');
+  res.end('hello hutul');
 });
 
 app.get('/:id', function(req, res) {
